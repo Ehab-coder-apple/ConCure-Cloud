8f2f88a06f684e1eaa9595c25585c0a0a282cfe6
Ehab khorshed <ehabkhorshed@Ehabs-MacBook-Air.local>
2025-09-15 22:20:01 +0300

    fix(patients): correctly prefill date_of_birth in Edit form with Y-m-d format

--- resources/views/patients/edit.blade.php
+++ resources/views/patients/edit.blade.php
@@ -73,7 +73,7 @@
                                         <label for="date_of_birth" class="form-label">{{ __('Date of Birth') }}</label>
                                         <input type="date" class="form-control @error('date_of_birth') is-invalid @enderror" 
                                                id="date_of_birth" name="date_of_birth" 
-                                               value="{{ old('date_of_birth', $patient->date_of_birth ?? '1990-01-01') }}">
+                                               value="{{ old('date_of_birth', $patient->date_of_birth ? $patient->date_of_birth->format('Y-m-d') : '') }}">
                                         @error('date_of_birth')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
