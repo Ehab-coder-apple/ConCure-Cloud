45f98d8956d260301b97c265c033ecb337821954
Augment Agent <augment-bot@users.noreply.github.com>
2025-11-02 23:16:43 +0300

    Checkup Templates: avoid route() for edit links in show view to bypass param mismatch; use url() path

--- resources/views/admin/checkup-templates/show.blade.php
+++ resources/views/admin/checkup-templates/show.blade.php
@@ -20,7 +20,7 @@
                         <i class="fas fa-arrow-left me-1"></i>
                         {{ __('Back to Templates') }}
                     </a>
-                    <a href="{{ route('admin.checkup-templates.edit', ['checkup_template' => $template->id]) }}" class="btn btn-primary">
+                    <a href="{{ url('/admin/checkup-templates/'.$template->id.'/edit') }}" class="btn btn-primary">
                         <i class="fas fa-edit me-1"></i>
                         {{ __('Edit Template') }}
                     </a>
@@ -200,7 +200,7 @@
                             <i class="fas fa-exclamation-triangle fa-2x text-warning mb-3"></i>
                             <h5 class="text-muted">{{ __('No Form Structure Defined') }}</h5>
                             <p class="text-muted">{{ __('This template does not have any form sections or fields configured.') }}</p>
-                            <a href="{{ route('admin.checkup-templates.edit', ['checkup_template' => $template->id]) }}" class="btn btn-primary">
+                            <a href="{{ url('/admin/checkup-templates/'.$template->id.'/edit') }}" class="btn btn-primary">
                                 <i class="fas fa-edit me-1"></i>
                                 {{ __('Configure Form Structure') }}
                             </a>
@@ -283,7 +283,7 @@
                 </div>
                 <div class="card-body">
                     <div class="btn-group me-2" role="group">
-                        <a href="{{ route('admin.checkup-templates.edit', ['checkup_template' => $template->id]) }}" class="btn btn-primary">
+                        <a href="{{ url('/admin/checkup-templates/'.$template->id.'/edit') }}" class="btn btn-primary">
                             <i class="fas fa-edit me-1"></i>
                             {{ __('Edit Template') }}
                         </a>
